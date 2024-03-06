7c0d7cdfc424bb9e981e80b9e939547ab05619b2
jra <julien.gault.50@gmail.com>
2024-03-06 15:25:06 +0100

    solution

--- MSLearn.CSharpConvertCast.Challenge/Program.cs
+++ MSLearn.CSharpConvertCast.Challenge/Program.cs
@@ -2,3 +2,19 @@
 CultureInfo.CurrentCulture = new CultureInfo("en-US");
 
 string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+
+decimal total = 0m;
+string message = "";
+
+foreach (string value in values)
+{
+    decimal numericValue;
+
+    if (decimal.TryParse(value, out numericValue))
+        total += numericValue;
+    else
+        message += value;
+}
+
+Console.WriteLine($"Message: {message}");
+Console.WriteLine($"Total: {total}");
